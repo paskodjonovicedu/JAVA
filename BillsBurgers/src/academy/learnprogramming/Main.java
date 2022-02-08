58b3bb3e026e3b3bf2f4218fa7076d4b4d5d9fc6
paskodjonovicedu <pasko.djonovic@udg.edu.me>
2022-02-08 13:55:41 +0100

    Initial commit

--- BillsBurgers/src/academy/learnprogramming/Main.java
+++ BillsBurgers/src/academy/learnprogramming/Main.java
@@ -10,5 +10,14 @@ public class Main {
         hamburger.addHamburgerAddition2("Lettuce",0.75);
         hamburger.addHamburgerAddition3("Cheese",1.12);
         System.out.println("Total burger price is " + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthAddition1("Lentils",3.41);
+        System.out.println("Total Healthy Burger price is " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger deluxeBurger = new DeluxeBurger();
+        deluxeBurger.addHamburgerAddition1("Should not do this",50);
+        deluxeBurger.itemizeHamburger();
     }
 }
