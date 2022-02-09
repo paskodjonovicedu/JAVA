597383c3938bfbc801cd798e3fe4fb50bf0b336b
paskodjonovicedu <pasko.djonovic@udg.edu.me>
2022-02-09 11:59:03 +0100

    Initial commit

--- SortedArray/academy/learnprogramming2/Main.java
+++ SortedArray/academy/learnprogramming2/Main.java
@@ -1,5 +1,6 @@
 package academy.learnprogramming2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,28 +9,46 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        printArray();
-
+        int[] myIntegers = getIntegers(5);
+        int[] sorted = sortIntegers(myIntegers);
+        printArray(sorted);
     }
-
-    private static int[] getIntegers(int number) {
-        System.out.println("Enter " + number + " integer values.\r");
-        int[] values = new int[number];
-        for (int i = 0; i < values.length; i++) {
-            values[i] = scanner.nextInt();
+    public static int[] getIntegers(int capacity) {
+        int[] array = new int[capacity];
+        System.out.println("Enter " + capacity + " integer values:\r");
+        for (int i=0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
         }
-        return values;
+        return array;
     }
 
-    int[] myIntegers = getIntegers(5);
-
-    private void printArray() {
-        for (int i = 0; i < myIntegers.length; i++) {
-            System.out.println("Element " + i + " contents " + myIntegers[i]);
+    public static void printArray(int[] array) {
+        for (int i = 0; i< array.length; i++) {
+            System.out.println("Element " + i + " contents " + array[i]);
         }
     }
 
-    private void sortIntegers() {
+    public static int[] sortIntegers(int[] array) {
+//        int[] sortedArray = new int[array.length];
+//        for (int i = 0; i < array.length; i++) {
+//            sortedArray[i] = array[i];
+//        }
 
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+
+        boolean flag = true;
+        int temp;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
     }
 }
